8b178b4de71cea6010cb71f06502adb86a7c1c8b
dylee <leeduyoung2002@gmail.com>
2021-07-11 19:16:03 +0900

    fix: 주석 추가

--- webrtc-basic/client/src/App.tsx
+++ webrtc-basic/client/src/App.tsx
@@ -1,20 +1,20 @@
-import './App.scss'
+import './App.scss';
 
-import io from 'socket.io-client'
-import React, { useEffect, useRef, useState } from 'react'
+import io from 'socket.io-client';
+import React, { useEffect, useRef, useState } from 'react';
 
 function App() {
   // RTCPeerConnection
-  const [pc, setPc] = useState<RTCPeerConnection>()
+  const [pc, setPc] = useState<RTCPeerConnection>();
 
   // Signaling Server와 통신할 socket
-  const [socket, setSocket] = useState<SocketIOClient.Socket>()
+  const [socket, setSocket] = useState<SocketIOClient.Socket>();
 
   // 본인의 video, audio를 재생할 video 태그의 ref
-  let localVideoRef = useRef<HTMLVideoElement>(null)
+  let localVideoRef = useRef<HTMLVideoElement>(null);
 
   // 상대의 video, audio를 재생할 video 태그의 ref
-  let remoteVideoRef = useRef<HTMLVideoElement>(null)
+  let remoteVideoRef = useRef<HTMLVideoElement>(null);
 
   const pc_config = {
     iceServers: [
@@ -27,7 +27,7 @@ function App() {
         urls: 'stun:stun.l.google.com:19302',
       },
     ],
-  }
+  };
 
   /**
    * Socket 수신 이벤트
@@ -38,39 +38,44 @@ function App() {
    * getCandidate: 본인 RTCPeerConnection의 IceCandidate로 상대방의 RTCIceCandidate를 설정한다
    */
   useEffect(() => {
-    let socketObj = io.connect('http://localhost:8080')
-    let peerConnectionObj = new RTCPeerConnection(pc_config)
+    let socketObj = io.connect('http://localhost:8080');
+    let peerConnectionObj = new RTCPeerConnection(pc_config);
 
-    socketObj.on('all_users', (allUsers: Array<{ id: string; email: string }>) => {
-      let len = allUsers.length
-      if (len > 0) {
-        // user에게 Offer signal을 보낸다
-        createOffer()
-      }
-    })
+    socketObj.on(
+      'all_users',
+      (allUsers: Array<{ id: string; email: string }>) => {
+        let len = allUsers.length;
+        if (len > 0) {
+          // user에게 Offer signal을 보낸다
+          createOffer();
+        }
+      },
+    );
 
     socketObj.on('getOffer', (sdp: RTCSessionDescription) => {
-      console.log('get offer, sdp: ', sdp)
+      console.log('get offer, sdp: ', sdp);
 
       // 해당 user에게 answer signal을 보낸다
-      createAnswer(sdp)
-    })
+      createAnswer(sdp);
+    });
 
     socketObj.on('getAnswer', (sdp: RTCSessionDescription) => {
-      console.log('get answer, sdp: ', sdp)
+      console.log('get answer, sdp: ', sdp);
 
       // 본인 RTCPeerConnection의 RemoteDescription으로 상대방의 RTCSessionDescription을 설정한다.
-      peerConnectionObj.setRemoteDescription(new RTCSessionDescription(sdp))
-    })
+      peerConnectionObj.setRemoteDescription(new RTCSessionDescription(sdp));
+    });
 
     socketObj.on('getCandidate', (candidate: RTCIceCandidateInit) => {
-      peerConnectionObj.addIceCandidate(new RTCIceCandidate(candidate)).then(() => {
-        console.log('candidate add success')
-      })
-    })
+      peerConnectionObj
+        .addIceCandidate(new RTCIceCandidate(candidate))
+        .then(() => {
+          console.log('candidate add success');
+        });
+    });
 
-    setSocket(socketObj)
-    setPc(peerConnectionObj)
+    setSocket(socketObj);
+    setPc(peerConnectionObj);
 
     // MediaStream 설정 및 RTCPeerConnection 이벤트
     navigator.mediaDevices
@@ -79,74 +84,86 @@ function App() {
         audio: true,
       })
       .then((stream) => {
-        if (localVideoRef.current) localVideoRef.current.srcObject = stream
+        if (localVideoRef.current) localVideoRef.current.srcObject = stream;
 
         stream.getTracks().forEach((track) => {
-          peerConnectionObj.addTrack(track, stream)
-        })
+          peerConnectionObj.addTrack(track, stream);
+        });
 
         // offer 또는 answer signal을 생성한후부터 본인의 icecandidate 정보 이벤트가 발생
         // offer 또는 answer를 보냈던 상대방에게 본인의 icecandidate 정보를 Signaling Server를 통해 보낸다
         peerConnectionObj.onicecandidate = (e) => {
           if (e.candidate) {
-            console.log('onicecandidate')
-            socketObj.emit('candidate', e.candidate)
+            console.log('onicecandidate');
+            socketObj.emit('candidate', e.candidate);
           }
-        }
+        };
 
         // ICE connection 상태가 변경됐을때 이벤트가 발생
         peerConnectionObj.oniceconnectionstatechange = (e) => {
-          console.log(e)
-        }
+          console.log(e);
+        };
 
         // 상대방의 RTCSessionDescription을 본인의 RTCPeerConnection 에서의 remoteSessionDescription으로 지정하면
         // 상대방의 데이터(video, audio track)에 대한 이벤트가 발생
         peerConnectionObj.ontrack = (ev) => {
-          console.log('add remotetrack success')
+          console.log('add remotetrack success');
 
           // 상대방의 데이터를 연결
-          if (remoteVideoRef.current) remoteVideoRef.current.srcObject = ev.streams[0]
-        }
+          if (remoteVideoRef.current)
+            remoteVideoRef.current.srcObject = ev.streams[0];
+        };
 
-        socketObj.emit('join_room', { room: '1234', email: 'sample@naver.com' })
+        // 방에 참여
+        socketObj.emit('join_room', {
+          room: '1234',
+          email: 'sample@naver.com',
+        });
       })
       .catch((error) => {
-        console.log(`getUserMedia error: ${error}`)
-      })
+        console.log(`getUserMedia error: ${error}`);
+      });
 
     // 상대방에게 offer signal 전달
     const createOffer = () => {
-      console.log('create offer')
+      console.log('create offer');
       peerConnectionObj
         .createOffer({ offerToReceiveAudio: true, offerToReceiveVideo: true })
         .then((sdp) => {
-          peerConnectionObj.setLocalDescription(new RTCSessionDescription(sdp))
-          socketObj.emit('offer', sdp)
+          peerConnectionObj.setLocalDescription(new RTCSessionDescription(sdp));
+          socketObj.emit('offer', sdp);
         })
         .catch((error) => {
-          console.log(error)
-        })
-    }
+          console.log(error);
+        });
+    };
 
     // 상대방에게 answer signal 전달
     const createAnswer = (sdp: RTCSessionDescription) => {
-      peerConnectionObj.setRemoteDescription(new RTCSessionDescription(sdp)).then(() => {
-        console.log('answer set remote description success')
-        peerConnectionObj
-          .createAnswer({ offerToReceiveVideo: true, offerToReceiveAudio: true })
-          .then((sdp1) => {
-            console.log('create answer')
-            peerConnectionObj.setLocalDescription(new RTCSessionDescription(sdp1))
+      peerConnectionObj
+        .setRemoteDescription(new RTCSessionDescription(sdp))
+        .then(() => {
+          console.log('answer set remote description success');
+          peerConnectionObj
+            .createAnswer({
+              offerToReceiveVideo: true,
+              offerToReceiveAudio: true,
+            })
+            .then((sdp1) => {
+              console.log('create answer');
+              peerConnectionObj.setLocalDescription(
+                new RTCSessionDescription(sdp1),
+              );
 
-            // answer signal로 나의 sdp를 보낸다
-            socketObj.emit('answer', sdp1)
-          })
-          .catch((error) => {
-            console.log(error)
-          })
-      })
-    }
-  }, [])
+              // answer signal로 나의 sdp를 보낸다
+              socketObj.emit('answer', sdp1);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+    };
+  }, []);
 
   return (
     <div>
@@ -173,7 +190,7 @@ function App() {
         autoPlay
       ></video>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
