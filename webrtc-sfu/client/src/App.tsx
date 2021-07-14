ece6812bfaff6037471eb26d62ea68b5c4c8fc5f
dylee <leeduyoung2002@gmail.com>
2021-07-14 11:56:18 +0900

    feat: 주석 추가

--- webrtc-sfu/client/src/App.tsx
+++ webrtc-sfu/client/src/App.tsx
@@ -1,17 +1,17 @@
-import './App.scss';
+import './App.scss'
 
-import React, { useEffect, useRef, useState } from 'react';
-import io from 'socket.io-client';
-import Video from './components/Video';
+import React, { useEffect, useRef, useState } from 'react'
+import io from 'socket.io-client'
+import Video from './components/Video'
 
 function App() {
-  const [socket, setSocket] = useState<SocketIOClient.Socket>();
-  const [users, setUsers] = useState<Array<IWebRTCUser>>([]);
+  const [socket, setSocket] = useState<SocketIOClient.Socket>()
+  const [users, setUsers] = useState<Array<IWebRTCUser>>([])
 
-  let localVideoRef = useRef<HTMLVideoElement>(null);
+  let localVideoRef = useRef<HTMLVideoElement>(null)
 
-  let sendPC: RTCPeerConnection;
-  let receivePCs: { [socketId: string]: RTCPeerConnection };
+  let sendPC: RTCPeerConnection
+  let receivePCs: { [socketId: string]: RTCPeerConnection }
 
   const pc_config = {
     iceServers: [
@@ -24,241 +24,285 @@ function App() {
         urls: 'stun:stun.l.google.com:19302',
       },
     ],
-  };
+  }
 
   useEffect(() => {
-    let newSocket = io.connect('http://localhost:8080');
-    let localStream: MediaStream;
+    let newSocket = io.connect('http://localhost:8080')
+    let localStream: MediaStream
 
     newSocket.on('userEnter', (data: { id: string }) => {
-      createReceivePC(data.id, newSocket);
-    });
+      console.log('[socket event called] userEnter, data: ', data)
+      createReceivePC(data.id, newSocket)
+    })
 
     newSocket.on('allUsers', (data: { users: Array<{ id: string }> }) => {
-      let len = data.users.length;
+      console.log('[socket event called] allUsers, data: ', data)
+      let len = data.users.length
       for (let i = 0; i < len; i++) {
-        createReceivePC(data.users[i].id, newSocket);
+        createReceivePC(data.users[i].id, newSocket)
       }
-    });
+    })
 
     newSocket.on('userExit', (data: { id: string }) => {
-      receivePCs[data.id].close();
-      delete receivePCs[data.id];
-      setUsers((users) => users.filter((user) => user.id !== data.id));
-    });
+      console.log('[socket event called] userExit, data: ', data)
+      receivePCs[data.id].close()
+      delete receivePCs[data.id]
+      setUsers((users) => users.filter((user) => user.id !== data.id))
+    })
 
-    newSocket.on(
-      'getSenderAnswer',
-      async (data: { sdp: RTCSessionDescription }) => {
-        try {
-          console.log('get sender answer');
-          console.log(data.sdp);
-          await sendPC.setRemoteDescription(
-            new RTCSessionDescription(data.sdp),
-          );
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    );
+    newSocket.on('getSenderAnswer', async (data: { sdp: RTCSessionDescription }) => {
+      try {
+        console.log('[socket event called] getSenderAnswer, data: ', data)
+        console.log(data.sdp)
+        await sendPC.setRemoteDescription(new RTCSessionDescription(data.sdp))
+      } catch (error) {
+        console.log(error)
+      }
+    })
 
-    newSocket.on(
-      'getSenderCandidate',
-      async (data: { candidate: RTCIceCandidateInit }) => {
-        try {
-          console.log('get sender candidate');
-          if (!data.candidate) return;
-          sendPC.addIceCandidate(new RTCIceCandidate(data.candidate));
-          console.log('candidate add success');
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    );
+    /**
+     * 해당 RTCPeerConnection의 remoteDescription으로 sdp 설정
+     */
+     newSocket.on('getReceiverAnswer', async (data: { id: string; sdp: RTCSessionDescription }) => {
+      try {
+        console.log('[socket event called] getReceiverAnswer, data: ', data)
+        console.log(`get socketID(${data.id})'s answer`)
+        let pc: RTCPeerConnection = receivePCs[data.id]
+        await pc.setRemoteDescription(data.sdp)
+        console.log(`socketID(${data.id})'s set remote sdp success`)
+      } catch (error) {
+        console.log(error)
+      }
+    })    
 
-    newSocket.on(
-      'getReceiverAnswer',
-      async (data: { id: string; sdp: RTCSessionDescription }) => {
-        try {
-          console.log(`get socketID(${data.id})'s answer`);
-          let pc: RTCPeerConnection = receivePCs[data.id];
-          await pc.setRemoteDescription(data.sdp);
-          console.log(`socketID(${data.id})'s set remote sdp success`);
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    );
+    newSocket.on('getSenderCandidate', async (data: { candidate: RTCIceCandidateInit }) => {
+      try {
+        console.log('[socket event called] getSenderCandidate, data: ', data)
+        if (!data.candidate) return
+        sendPC.addIceCandidate(new RTCIceCandidate(data.candidate))
+        console.log('candidate add success')
+      } catch (error) {
+        console.log(error)
+      }
+    })
 
     newSocket.on(
       'getReceiverCandidate',
       async (data: { id: string; candidate: RTCIceCandidateInit }) => {
+        console.log('[socket event called] getReceiverCandidate, data: ', data)
         try {
-          console.log(data);
-          console.log(`get socketID(${data.id})'s candidate`);
-          let pc: RTCPeerConnection = receivePCs[data.id];
-          if (!data.candidate) return;
-          pc.addIceCandidate(new RTCIceCandidate(data.candidate));
-          console.log(`socketID(${data.id})'s candidate add success`);
+          console.log(data)
+          console.log(`get socketID(${data.id})'s candidate`)
+          let pc: RTCPeerConnection = receivePCs[data.id]
+          if (!data.candidate) return
+          pc.addIceCandidate(new RTCIceCandidate(data.candidate))
+          console.log(`socketID(${data.id})'s candidate add success`)
         } catch (error) {
-          console.log(error);
+          console.log(error)
         }
-      },
-    );
+      }
+    )
 
-    setSocket(newSocket);
+    setSocket(newSocket)
 
     navigator.mediaDevices
       .getUserMedia({
-        audio: true,
+        audio: false,
         video: {
           width: 240,
           height: 240,
         },
       })
       .then((stream) => {
-        if (localVideoRef.current) localVideoRef.current.srcObject = stream;
+        if (localVideoRef.current) localVideoRef.current.srcObject = stream
 
-        localStream = stream;
+        localStream = stream
 
-        sendPC = createSenderPeerConnection(newSocket, localStream);
-        createSenderOffer(newSocket);
+        sendPC = createSenderPeerConnection(newSocket, localStream)
+        createSenderOffer(newSocket)
 
         newSocket.emit('joinRoom', {
           id: newSocket.id,
           roomID: '1234',
-        });
+        })
       })
       .catch((error) => {
-        console.log(`getUserMedia error: ${error}`);
-      });
-  }, []);
+        console.log(`getUserMedia error: ${error}`)
+      })
+  }, [])
 
+  /**
+   * room에 참가한 다른 user들의 MediaStream을 받을 RTCPeerConnection을 생성하고 서버에 offer를 보냄
+   *
+   * @param id
+   * @param newSocket
+   */
   const createReceivePC = (id: string, newSocket: SocketIOClient.Socket) => {
-    try {
-      console.log(`socketID(${id}) user entered`);
-      let pc = createReceiverPeerConnection(id, newSocket);
-      createReceiverOffer(pc, newSocket, id);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    console.log('[func called] createReceivePC')
 
+    try {
+      console.log(`socketID(${id}) user entered`)
+      let pc = createReceiverPeerConnection(id, newSocket)
+      createReceiverOffer(pc, newSocket, id)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  /**
+   * 자신의 MediaStream을 서버에게 보낼 RTCPeerConnection의 Offer를 생성
+   * RTCSessionDescription을 본인의 RTCPeerConnection의 localDescription에 지정
+   * RTCSessionDescription을 소켓을 통해서 서버로 전송
+   *
+   * @param newSocket
+   */
   const createSenderOffer = async (newSocket: SocketIOClient.Socket) => {
+    console.log('[func called] createSenderOffer')
     try {
       let sdp = await sendPC.createOffer({
         offerToReceiveAudio: false,
         offerToReceiveVideo: false,
-      });
-      console.log('create sender offer success');
-      await sendPC.setLocalDescription(new RTCSessionDescription(sdp));
+      })
+      console.log('create sender offer success')
+      await sendPC.setLocalDescription(new RTCSessionDescription(sdp))
 
       newSocket.emit('senderOffer', {
         sdp,
         senderSocketID: newSocket.id,
         roomID: '1234',
-      });
+      })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
+  /**
+   * senderSocketID user의 MediaStream을 전송받을 RTCPeerConnection의 offer를 생성
+   *
+   * @param pc
+   * @param newSocket
+   * @param senderSocketID
+   */
   const createReceiverOffer = async (
     pc: RTCPeerConnection,
     newSocket: SocketIOClient.Socket,
-    senderSocketID: string,
+    senderSocketID: string
   ) => {
+    console.log('[func called] createReceiverOffer')
     try {
       let sdp = await pc.createOffer({
         offerToReceiveAudio: true,
         offerToReceiveVideo: true,
-      });
-      console.log('create receiver offer success');
-      await pc.setLocalDescription(new RTCSessionDescription(sdp));
+      })
+      console.log('create receiver offer success')
+      await pc.setLocalDescription(new RTCSessionDescription(sdp))
 
       newSocket.emit('receiverOffer', {
-        sdp,
-        receiverSocketID: newSocket.id,
-        senderSocketID,
+        sdp, // 나의 sdp
+        receiverSocketID: newSocket.id, // 나의 Socket id
+        senderSocketID, // 상대방의 socket id
         roomID: '1234',
-      });
+      })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
+  /**
+   * 자신의 MediaStream을 서버로 보내기 위한 RTCPeerConnection을 생성하고 localStream을 등록
+   */
   const createSenderPeerConnection = (
     newSocket: SocketIOClient.Socket,
-    localStream: MediaStream,
+    localStream: MediaStream
   ): RTCPeerConnection => {
-    let pc = new RTCPeerConnection(pc_config);
+    console.log('[func called] createSenderPeerConnection')
+    let pc = new RTCPeerConnection(pc_config)
 
+    /**
+     * offer 또는 answer signal을 생성한 후 본인의 RTCIceCandidate 정보 이벤트 발생
+     *
+     * @param e
+     */
     pc.onicecandidate = (e) => {
       if (e.candidate) {
-        console.log('sender PC onicecandidate');
+        console.log('[SenderPeerConnection event called] onicecandidate')
         newSocket.emit('senderCandidate', {
           candidate: e.candidate,
           senderSocketID: newSocket.id,
-        });
+        })
       }
-    };
+    }
 
+    // ICE connection 상태가 변경되었을때 호출
     pc.oniceconnectionstatechange = (e) => {
-      console.log(e);
-    };
+      console.log('[SenderPeerConnection event called] oniceconnectionstatechange')
+      console.log(e)
+    }
 
     if (localStream) {
-      console.log('localstream add');
+      console.log('localstream add')
       localStream.getTracks().forEach((track) => {
-        pc.addTrack(track, localStream);
-      });
+        pc.addTrack(track, localStream)
+      })
     } else {
-      console.log('no local stream');
+      console.log('no local stream')
     }
 
     // return pc
-    return pc;
-  };
+    return pc
+  }
 
+  /**
+   * SocketID user의 MediaStream을 받기 위한 RTCPeerConnection 생성
+   *
+   * @param socketID
+   * @param newSocket
+   * @returns
+   */
   const createReceiverPeerConnection = (
     socketID: string,
-    newSocket: SocketIOClient.Socket,
+    newSocket: SocketIOClient.Socket
   ): RTCPeerConnection => {
-    let pc = new RTCPeerConnection(pc_config);
+    console.log('[func called] createReceiverPeerConnection')
+
+    let pc = new RTCPeerConnection(pc_config)
 
     // add pc to peerConnections object
-    receivePCs = { ...receivePCs, [socketID]: pc };
+    receivePCs = { ...receivePCs, [socketID]: pc }
 
     pc.onicecandidate = (e) => {
       if (e.candidate) {
-        console.log('receiver PC onicecandidate');
+        console.log('[ReceiverPeerConnection event called] onicecandidate')
+        console.log('receiver PC onicecandidate')
         newSocket.emit('receiverCandidate', {
           candidate: e.candidate,
           receiverSocketID: newSocket.id,
           senderSocketID: socketID,
-        });
+        })
       }
-    };
+    }
 
     pc.oniceconnectionstatechange = (e) => {
-      console.log(e);
-    };
+      console.log('[ReceiverPeerConnection event called] oniceconnectionstatechange')
+      console.log(e)
+    }
 
     pc.ontrack = (e) => {
-      console.log('ontrack success');
-      setUsers((oldUsers) => oldUsers.filter((user) => user.id !== socketID));
+      console.log('[ReceiverPeerConnection event called] ontrack')
+      console.log('ontrack success')
+      setUsers((oldUsers) => oldUsers.filter((user) => user.id !== socketID))
       setUsers((oldUsers) => [
         ...oldUsers,
         {
           id: socketID,
           stream: e.streams[0],
         },
-      ]);
-    };
+      ])
+    }
 
     // return pc
-    return pc;
-  };
+    return pc
+  }
 
   return (
     <div>
@@ -274,10 +318,10 @@ function App() {
         autoPlay
       ></video>
       {users.map((user, index) => {
-        return <Video key={index} stream={user.stream} />;
+        return <Video key={index} stream={user.stream} />
       })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- webrtc-sfu/server/server.js
+++ webrtc-sfu/server/server.js
@@ -1,18 +1,18 @@
-let http = require('http');
-let express = require('express');
-let cors = require('cors');
-let socketio = require('socket.io');
-let wrtc = require('wrtc');
+let http = require('http')
+let express = require('express')
+let cors = require('cors')
+let socketio = require('socket.io')
+let wrtc = require('wrtc')
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
 
-app.use(cors());
+app.use(cors())
 
-let receiverPCs = {};
-let senderPCs = {};
-let users = {};
-let socketToRoom = {};
+let receiverPCs = {}
+let senderPCs = {}
+let users = {}
+let socketToRoom = {}
 
 const pc_config = {
   iceServers: [
@@ -25,236 +25,258 @@ const pc_config = {
       urls: 'stun:stun.l.google.com:19302',
     },
   ],
-};
+}
 
 const isIncluded = (array, id) => {
-  let len = array.length;
+  console.log('[func called] isIncluded')
+  let len = array.length
   for (let i = 0; i < len; i++) {
-    if (array[i].id === id) return true;
+    if (array[i].id === id) return true
   }
-  return false;
-};
+  return false
+}
 
 const createReceiverPeerConnection = (socketID, socket, roomID) => {
-  let pc = new wrtc.RTCPeerConnection(pc_config);
+  console.log('[func called] createReceiverPeerConnection')
 
-  if (receiverPCs[socketID]) receiverPCs[socketID] = pc;
-  else receiverPCs = { ...receiverPCs, [socketID]: pc };
+  let pc = new wrtc.RTCPeerConnection(pc_config)
+
+  if (receiverPCs[socketID]) receiverPCs[socketID] = pc
+  else receiverPCs = { ...receiverPCs, [socketID]: pc }
 
   pc.onicecandidate = (e) => {
+    console.log('[ReceiverPeerConnection event called] onicecandidate')
     //console.log(`socketID: ${socketID}'s receiverPeerConnection icecandidate`);
     socket.to(socketID).emit('getSenderCandidate', {
       candidate: e.candidate,
-    });
-  };
+    })
+  }
 
   pc.oniceconnectionstatechange = (e) => {
     //console.log(e);
-  };
+    console.log('[ReceiverPeerConnection event called] oniceconnectionstatechange')
+  }
 
   pc.ontrack = (e) => {
+    console.log('[ReceiverPeerConnection event called] ontrack')
+
     if (users[roomID]) {
       if (!isIncluded(users[roomID], socketID)) {
         users[roomID].push({
           id: socketID,
           stream: e.streams[0],
-        });
-      } else return;
+        })
+      } else return
     } else {
       users[roomID] = [
         {
           id: socketID,
           stream: e.streams[0],
         },
-      ];
+      ]
     }
-    socket.broadcast.to(roomID).emit('userEnter', { id: socketID });
-  };
+    socket.broadcast.to(roomID).emit('userEnter', { id: socketID })
+  }
 
-  return pc;
-};
+  return pc
+}
 
-const createSenderPeerConnection = (
-  receiverSocketID,
-  senderSocketID,
-  socket,
-  roomID,
-) => {
-  let pc = new wrtc.RTCPeerConnection(pc_config);
+const createSenderPeerConnection = (receiverSocketID, senderSocketID, socket, roomID) => {
+  console.log('[func called] createSenderPeerConnection')
+  let pc = new wrtc.RTCPeerConnection(pc_config)
 
   if (senderPCs[senderSocketID]) {
-    senderPCs[senderSocketID].filter((user) => user.id !== receiverSocketID);
-    senderPCs[senderSocketID].push({ id: receiverSocketID, pc: pc });
+    senderPCs[senderSocketID].filter((user) => user.id !== receiverSocketID)
+    senderPCs[senderSocketID].push({ id: receiverSocketID, pc: pc })
   } else
     senderPCs = {
       ...senderPCs,
       [senderSocketID]: [{ id: receiverSocketID, pc: pc }],
-    };
+    }
 
   pc.onicecandidate = (e) => {
+    console.log('[SenderPeerConnection event called] onicecandidate')
+
     //console.log(`socketID: ${receiverSocketID}'s senderPeerConnection icecandidate`);
     socket.to(receiverSocketID).emit('getReceiverCandidate', {
       id: senderSocketID,
       candidate: e.candidate,
-    });
-  };
+    })
+  }
 
   pc.oniceconnectionstatechange = (e) => {
     //console.log(e);
-  };
+    console.log('[SenderPeerConnection event called] oniceconnectionstatechange')
+  }
 
-  const sendUser = users[roomID].filter((user) => user.id === senderSocketID);
+  const sendUser = users[roomID].filter((user) => user.id === senderSocketID)
   sendUser[0].stream.getTracks().forEach((track) => {
-    pc.addTrack(track, sendUser[0].stream);
-  });
+    pc.addTrack(track, sendUser[0].stream)
+  })
 
-  return pc;
-};
+  return pc
+}
 
 const getOtherUsersInRoom = (socketID, roomID) => {
-  let allUsers = [];
+  console.log('[func called] getOtherUsersInRoom')
 
-  if (!users[roomID]) return allUsers;
+  let allUsers = []
 
-  let len = users[roomID].length;
+  if (!users[roomID]) return allUsers
+
+  let len = users[roomID].length
   for (let i = 0; i < len; i++) {
-    if (users[roomID][i].id === socketID) continue;
-    allUsers.push({ id: users[roomID][i].id });
+    if (users[roomID][i].id === socketID) continue
+    allUsers.push({ id: users[roomID][i].id })
   }
 
-  return allUsers;
-};
+  return allUsers
+}
 
 const deleteUser = (socketID, roomID) => {
-  let roomUsers = users[roomID];
-  if (!roomUsers) return;
-  roomUsers = roomUsers.filter((user) => user.id !== socketID);
-  users[roomID] = roomUsers;
+  console.log('[func called] deleteUser')
+
+  let roomUsers = users[roomID]
+  if (!roomUsers) return
+  roomUsers = roomUsers.filter((user) => user.id !== socketID)
+  users[roomID] = roomUsers
   if (roomUsers.length === 0) {
-    delete users[roomID];
+    delete users[roomID]
   }
-  delete socketToRoom[socketID];
-};
+  delete socketToRoom[socketID]
+}
 
 const closeRecevierPC = (socketID) => {
-  if (!receiverPCs[socketID]) return;
+  console.log('[func called] closeRecevierPC')
 
-  receiverPCs[socketID].close();
-  delete receiverPCs[socketID];
-};
+  if (!receiverPCs[socketID]) return
+
+  receiverPCs[socketID].close()
+  delete receiverPCs[socketID]
+}
 
 const closeSenderPCs = (socketID) => {
-  if (!senderPCs[socketID]) return;
+  console.log('[func called] closeSenderPCs')
 
-  let len = senderPCs[socketID].length;
+  if (!senderPCs[socketID]) return
+
+  let len = senderPCs[socketID].length
   for (let i = 0; i < len; i++) {
-    senderPCs[socketID][i].pc.close();
-    let _senderPCs = senderPCs[senderPCs[socketID][i].id];
-    let senderPC = _senderPCs.filter((sPC) => sPC.id === socketID);
+    senderPCs[socketID][i].pc.close()
+    let _senderPCs = senderPCs[senderPCs[socketID][i].id]
+    let senderPC = _senderPCs.filter((sPC) => sPC.id === socketID)
     if (senderPC[0]) {
-      senderPC[0].pc.close();
-      senderPCs[senderPCs[socketID][i].id] = _senderPCs.filter(
-        (sPC) => sPC.id !== socketID,
-      );
+      senderPC[0].pc.close()
+      senderPCs[senderPCs[socketID][i].id] = _senderPCs.filter((sPC) => sPC.id !== socketID)
     }
   }
 
-  delete senderPCs[socketID];
-};
+  delete senderPCs[socketID]
+}
 
-const io = socketio.listen(server);
+const io = socketio.listen(server)
 
 io.sockets.on('connection', (socket) => {
   socket.on('joinRoom', (data) => {
+    console.log('[socket event called] joinRoom, data: ', data)
+
     try {
-      let allUsers = getOtherUsersInRoom(data.id, data.roomID);
-      io.to(data.id).emit('allUsers', { users: allUsers });
+      let allUsers = getOtherUsersInRoom(data.id, data.roomID)
+      io.to(data.id).emit('allUsers', { users: allUsers })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  });
+  })
 
   socket.on('senderOffer', async (data) => {
+    console.log('[socket event called] senderOffer')
+
     try {
-      socketToRoom[data.senderSocketID] = data.roomID;
-      let pc = createReceiverPeerConnection(
-        data.senderSocketID,
-        socket,
-        data.roomID,
-      );
-      await pc.setRemoteDescription(data.sdp);
+      socketToRoom[data.senderSocketID] = data.roomID
+      let pc = createReceiverPeerConnection(data.senderSocketID, socket, data.roomID)
+      await pc.setRemoteDescription(data.sdp)
       let sdp = await pc.createAnswer({
         offerToReceiveAudio: true,
         offerToReceiveVideo: true,
-      });
-      await pc.setLocalDescription(sdp);
-      socket.join(data.roomID);
-      io.to(data.senderSocketID).emit('getSenderAnswer', { sdp });
+      })
+      await pc.setLocalDescription(sdp)
+      socket.join(data.roomID)
+      io.to(data.senderSocketID).emit('getSenderAnswer', { sdp })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  });
+  })
 
   socket.on('senderCandidate', async (data) => {
-    try {
-      let pc = receiverPCs[data.senderSocketID];
-      await pc.addIceCandidate(new wrtc.RTCIceCandidate(data.candidate));
-    } catch (error) {
-      console.log(error);
-    }
-  });
+    console.log('[socket event called] senderCandidate, data: ', data)
 
+    try {
+      let pc = receiverPCs[data.senderSocketID]
+      await pc.addIceCandidate(new wrtc.RTCIceCandidate(data.candidate))
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  /**
+   * receiverSocketID를 socket id로 가지는 user가 senderSocketID를 socket id로 가지는
+   * user의 MediaStream을 받기 위한 RTCPeerConnection의 offer를 서버가 받고 answer를 보냄
+   */
   socket.on('receiverOffer', async (data) => {
+    console.log('[socket event called] receiverOffer, data: ', data)
+
     try {
       let pc = createSenderPeerConnection(
         data.receiverSocketID,
         data.senderSocketID,
         socket,
-        data.roomID,
-      );
-      await pc.setRemoteDescription(data.sdp);
+        data.roomID
+      )
+      await pc.setRemoteDescription(data.sdp)
       let sdp = await pc.createAnswer({
         offerToReceiveAudio: false,
         offerToReceiveVideo: false,
-      });
-      await pc.setLocalDescription(sdp);
+      })
+      await pc.setLocalDescription(sdp)
       io.to(data.receiverSocketID).emit('getReceiverAnswer', {
         id: data.senderSocketID,
         sdp,
-      });
+      })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  });
+  })
 
   socket.on('receiverCandidate', async (data) => {
+    console.log('[socket event called] receiverCandidate, data: ', data)
+
     try {
       const senderPC = senderPCs[data.senderSocketID].filter(
-        (sPC) => sPC.id === data.receiverSocketID,
-      );
-      await senderPC[0].pc.addIceCandidate(
-        new wrtc.RTCIceCandidate(data.candidate),
-      );
+        (sPC) => sPC.id === data.receiverSocketID
+      )
+      await senderPC[0].pc.addIceCandidate(new wrtc.RTCIceCandidate(data.candidate))
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  });
+  })
 
   socket.on('disconnect', () => {
+    console.log('[socket event called] disconnect, data: ', data)
+
     try {
-      let roomID = socketToRoom[socket.id];
+      let roomID = socketToRoom[socket.id]
 
-      deleteUser(socket.id, roomID);
-      closeRecevierPC(socket.id);
-      closeSenderPCs(socket.id);
+      deleteUser(socket.id, roomID)
+      closeRecevierPC(socket.id)
+      closeSenderPCs(socket.id)
 
-      socket.broadcast.to(roomID).emit('userExit', { id: socket.id });
+      socket.broadcast.to(roomID).emit('userExit', { id: socket.id })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  });
-});
+  })
+})
 
 server.listen(process.env.PORT || 8080, () => {
-  console.log('server running on 8080');
-});
+  console.log('server running on 8080')
+})
